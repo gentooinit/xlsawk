ef0f803d0b88b043eeece940bc7cfd711ec9ebf5
init0 <gentooinit@hotmail.com>
2015-12-14 17:41:00 +0800

    Fixed one more column bug
    Fixed text cell read as CELL_TYPE_NUMERIC
    Add merge cell reading support

--- src/org/init0/Awk.java
+++ src/org/init0/Awk.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
@@ -18,102 +19,138 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
  */
 
 public class Awk {
-	public enum ExcelType { XLS, XLSX };
-	
-	public Awk(OutputStream _os, InputStream _is, ExcelType type)
-			throws IOException
-	{
+	public enum ExcelType {
+		XLS, XLSX
+	};
+
+	public Awk(OutputStream _os, InputStream _is, ExcelType type) throws IOException {
 		os = _os;
 		is = _is;
-		
+
 		if (type == ExcelType.XLS) {
-				wb = new HSSFWorkbook(is);
+			wb = new HSSFWorkbook(is);
 		} else {
-				wb = new XSSFWorkbook(is);
+			wb = new XSSFWorkbook(is);
 		}
-		
+
 		NS = wb.getNumberOfSheets();
-		
+
 		sheet = new HashMap<String, Sheet>();
 		for (int i = 0; i < NS; ++i) {
 			sheet.put(wb.getSheetName(i), wb.getSheetAt(i));
 		}
-		
+
 		/* Default sheet */
 		AS = wb.getSheetAt(wb.getActiveSheetIndex());
-		
+
 		field = new ArrayList<String>();
 	}
-	
-	public void begin() {}
-	public void end() {}
-	protected void each() {}
-	
-	public void loop()
-	{
+
+	public void print(String str) {
+		System.out.print(str);
+	}
+
+	/* For Runnable Jar */
+	public static void main(String[] args) {
+	}
+
+	public void begin() {
+	}
+
+	public void end() {
+	}
+
+	protected void each() {
+	}
+
+	public void loop() {
 		int i, j;
-		
-		if (AS == null) {
+
+		if (AS == null || AS.getPhysicalNumberOfRows() == 0) {
 			return;
 		}
-		
+
 		for (i = 0; i <= AS.getLastRowNum(); ++i) {
 			NR++;
 			Row row = AS.getRow(i);
-			
+
 			field.clear();
-			
-			if (row != null) {			
-				for (j = 0; j <= row.getLastCellNum(); ++j) {
+
+			if (row != null) {
+				cellLoop: for (j = 0; j < row.getLastCellNum(); ++j) {
 					Cell cell = row.getCell(j);
-					
-					if (cell == null) {
-						field.add("");
-					} else {
-						String literal;
-						
-						switch (cell.getCellType()) {
-						case Cell.CELL_TYPE_FORMULA:
-							literal = cell.getCellFormula();
-							break;
-						case Cell.CELL_TYPE_NUMERIC:
-							literal = Double.toString(cell.getNumericCellValue());
-							break;
-						case Cell.CELL_TYPE_STRING:
-							literal = cell.getStringCellValue();
-							break;
-						default:
-							literal = cell.toString();
-							break;
+
+					CellRangeAddress range;
+					for (int k = 0; k < AS.getNumMergedRegions(); ++k) {
+						range = AS.getMergedRegion(k);
+
+						if (range.isInRange(i, j) == true) {
+							field.add(getMergeContent(range));
+							continue cellLoop;
 						}
-						field.add(literal.replaceAll("\n", ""));
 					}
+
+					field.add(getContent(cell));
 				}
 			}
-			
+
 			NF = field.size();
-			
+
 			each();
 		}
 	}
-	
-	public void print(String str)
-	{
-		System.out.print(str);
-	}
-	
-	/* For Runnable Jar */
-	public static void main(String[] args) {}
 
-	protected int NS;                               /* Number of Sheet */
-	protected int NR;                               /* Number of Record */
-	protected int NF;                               /* Number of Field */
-	protected Sheet AS = null;                      /* Active Sheet */
+	private String getContent(Cell cell) {
+		String literal = "";
+
+		if (cell != null) {
+			switch (cell.getCellType()) {
+			case Cell.CELL_TYPE_FORMULA:
+				literal = cell.getCellFormula();
+				break;
+			case Cell.CELL_TYPE_NUMERIC:
+				cell.setCellType(Cell.CELL_TYPE_STRING);
+				literal = cell.getStringCellValue();
+
+				/* POI Bug */
+				// literal = Double.toString(cell.getNumericCellValue());
+				break;
+			case Cell.CELL_TYPE_STRING:
+				literal = cell.getStringCellValue();
+				break;
+			default:
+				literal = cell.toString();
+				break;
+			}
+			literal.replaceAll("\n", "");
+		}
+
+		return literal;
+	}
+
+	private String getMergeContent(CellRangeAddress range) {
+		String literal = "";
+
+		int rowInx = range.getFirstRow();
+		int colInx = range.getFirstColumn();
+
+		Row row = AS.getRow(rowInx);
+		if (row != null) {
+			Cell cell = row.getCell(colInx);
+			literal = getContent(cell);
+		}
+
+		return literal;
+	}
+
+	protected int NS;                                  /* Number of Sheet */
+	protected int NR;                                  /* Number of Record */
+	protected int NF;                                  /* Number of Field */
+	protected Sheet AS = null;                         /* Active Sheet */
 	protected ArrayList<String> field;
 	protected Map<String, Sheet> sheet;
-	
+
 	private OutputStream os = null;
 	private InputStream is = null;
 	private Workbook wb = null;
 }
-
